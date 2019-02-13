544c7f92efb7dd8ab8c7e65d89584601bd3258eb
BibiChen <yongbing923@gmail.com>
2019-02-13 23:06:08 +0800

    Vue 学习

---
+++ VueDemo/src-base/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <img class="logo" src="./assets/logo.png" alt="logo">
+    <!-- 3. 使用组件标签 -->
+    <HelloWorld/>
+  </div>
+</template>
+
+<script>
+  // 1. 引入组件
+  import HelloWorld from './component/HelloWorld'
+
+  export default {
+    // 2. 映射组件标签
+    components: {
+      HelloWorld
+    },
+
+    data () {
+
+    }
+  }
+</script>
+
+<style>
+  .logo {
+    width: 200px;
+    height: 200px;
+  }
+</style>
---
+++ VueDemo/src/App.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="todo-container">
+    <div class="todo-wrap">
+      <TodoHeader :addTodo="addTodo"/>
+      <TodoList :todos="todos" :deleteTodo="deleteTodo"/>
+      <TodoFooter :todos="todos" :deleteCompleteTodos="deleteCompleteTodos"
+                  :selectAllTodos="selectAllTodos"/>
+
+    </div>
+  </div>
+</template>
+
+<script>
+  import TodoHeader from './components/TodoHeader'
+  import TodoList from './components/TodoList'
+  import TodoFooter from './components/TodoFooter'
+
+  export default {
+    data() {
+      return {
+        todos: [
+          {title: '吃饭', complete: false},
+          {title: '睡觉', complete: true},
+          {title: 'coding', complete: false}
+        ]
+      }
+    },
+
+    methods: {
+      // 添加 todo
+      addTodo(todo) {
+        this.todos.unshift(todo);
+      },
+      // 删除
+      deleteTodo(index) {
+        this.todos.splice(index, 1);
+      },
+      // 删除完成的 todos
+      deleteCompleteTodos() {
+        this.todos = this.todos.filter(todo => !todo.complete);
+      },
+      // 全选/全不选
+      selectAllTodos(check) {
+        this.todos.forEach(todo => todo.complete = check);
+      }
+    },
+
+    components: {
+      TodoHeader,
+      TodoList,
+      TodoFooter
+    }
+  }
+</script>
+
+<style scoped>
+  .todo-container {
+    width: 600px;
+    margin: 0 auto;
+  }
+
+  .todo-container .todo-wrap {
+    padding: 10px;
+    border: 1px solid #ddd;
+    border-radius: 5px;
+  }
+</style>
