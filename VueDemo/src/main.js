04bed324cfa63da863b83b2a7f39745ed7a03ed9
BibiChen <yongbing923@gmail.com>
2019-02-14 16:38:50 +0800

    UI 组件库 mint-ui

--- VueDemo/src/main.js
+++ VueDemo/src/main.js
@@ -1,11 +1,6 @@
 import Vue from 'vue'
 import App from './App'
-import {Button} from 'mint-ui'
 
-// 使用 mint-ui
-// 1. 注册成标签(全局)
-// Vue.component('mt-button', Button)
-Vue.component(Button.name, Button)
 
 new Vue({
   el: '#app',
